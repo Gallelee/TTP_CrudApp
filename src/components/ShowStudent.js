b27459e33bf8ee8deedaa647e1b8b5ef6e81e4e6
Gallelee <thimotus@gmail.com>
2022-01-21 14:49:08 -0500

    Add placeholder image for studen page

--- src/components/ShowStudent.js
+++ src/components/ShowStudent.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import {Link} from "react-router-dom"
 import Axios from "axios"
+import noImage from "../images/noImage.png"
 
 export default function ShowStudent(){
     const[student, setStudent] = useState(null)
@@ -16,8 +17,9 @@ export default function ShowStudent(){
     },[])
     
     return(
-        <div id="student-card">
+        <div id="student-page">
             <h1>im the showstudent component</h1>
+            <img src={noImage} alt="No image image"/> {/*Placer holder image */} 
             <h1>{student? student.data.name: "nothing to see here yet"}</h1>
             <h4>GPA: N/A</h4>
             <Link to="/EditStudent">EDIT</Link>
