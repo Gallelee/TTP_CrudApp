cf7b199c4acf6c7652ec6a6309c54015b9c9e6dd
Gallelee <thimotus@gmail.com>
2022-01-21 15:08:36 -0500

    Add all student display functionality

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,11 +1,34 @@
-import React from "react"
+import React, { useEffect , useState } from "react"
 import ShowStudent from "./ShowStudent"
+import Axios from "axios"
+import noImage from "../images/noImage.png"
 
 export default function Students(){
+
+    const [allStudents, setAllStudents] = useState(null)
+
+
+    useEffect(()=>{
+        async function getAllStudents(){
+            const all = await Axios.get("https://jsonplaceholder.typicode.com/users")
+            console.log(all)
+            setAllStudents(all)
+        }
+        getAllStudents()
+    }, [])
     return(
         <div id="students">
-            <h1>Nothing to see here yet</h1>
-            <ShowStudent/>
+            {allStudents? allStudents.data
+            .map(elm => {
+                return(
+                    <div className="mini-student-card">
+                        <img src={noImage}/>
+                        <h4>{elm.name}</h4>
+                    </div>
+                )
+            }) : "Nothing to see here"
+        }
+            
         </div>
     )
         
